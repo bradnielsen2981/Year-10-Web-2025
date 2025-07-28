3195400b7f4d9b33bd0cf66176b8d54ef3efa21f
bradnielsen2981 <brad_nielsen29@hotmail.com>
2025-07-29 09:26:16 +1000

    commit

--- myscript.js
+++ myscript.js
@@ -1,1 +1,10 @@
 alert("MY SCRIPT HAS LOADED");
+
+function showRating()
+{
+    rating.innerHTML = "<p>This movie sucked! It was terrible! The superdog was annoying. It tried too hard to be funny and failed miserably. The plot was predictable and boring. I would not recommend this movie to anyone. <b>Rating 1/5</b></p>";
+    rating.style = "background-colour:red";
+}
+
+//JAVASCRIPT EVENTS 
+ratingbutton.onclick = showRating;
