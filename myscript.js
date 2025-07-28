cd50b17175213d7fd6f1fc5021f00e7b47cd65e6
bradnielsen2981 <brad_nielsen29@hotmail.com>
2025-07-29 09:38:43 +1000

    commit

--- myscript.js
+++ myscript.js
@@ -1,10 +1,19 @@
 alert("MY SCRIPT HAS LOADED");
-
 function showRating()
 {
-    rating.innerHTML = "<p>This movie sucked! It was terrible! The superdog was annoying. It tried too hard to be funny and failed miserably. The plot was predictable and boring. I would not recommend this movie to anyone. <b>Rating 1/5</b></p>";
-    rating.style = "background-colour:red";
+    rating.innerHTML= "<p>This movie sucked! It was terrible! The superdog was annoying. It tried too hard to be funny and failed miserably. The plot was predictable and boring. I would not recommend this movie to anyone. <b>Rating 1/5</b></p>";
+    rating.style = "background-color:red";
 }
-
 //JAVASCRIPT EVENTS 
 ratingbutton.onclick = showRating;
+man2.style = "display:none";
+man1.onmouseover = showMan2;
+function showMan2()
+{
+    man2.style = "display:block";
+}
+man1.onmouseout = hideMan2;
+function hideMan2()
+{
+    man2.style = "display:none";
+}
